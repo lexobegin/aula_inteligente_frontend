905d949f75fbc821df3c38f832129e36be84dd8d
Alex Orellana <lexorel88@gmail.com>
2025-06-03 18:22:47 -0400

    crud de usuario

--- src/app/business/usuario/usuario/registerusuario/registerusuario.component.ts
+++ src/app/business/usuario/usuario/registerusuario/registerusuario.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, Router } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth.service';
+//import { ToastService } from '../../../../core/services/toast.service';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+declare var $: any; // Para usar jQuery con Bootstrap 4.6
 
 @Component({
   selector: 'app-registerusuario',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './registerusuario.component.html',
-  styleUrl: './registerusuario.component.css'
+  styleUrl: './registerusuario.component.css',
 })
-export class RegisterusuarioComponent {
+export class RegisterusuarioComponent implements AfterViewInit {
+  usuario = {
+    username: '',
+    email: '',
+    first_name: '',
+    last_name: '',
+    rol: '',
+    password: '',
+  };
 
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngAfterViewInit() {
+    // Inicializa el toast al cargar el componente
+    $('#toast').toast({ autohide: true, delay: 3000 });
+  }
+
+  crearUsuario() {
+    this.authService.crearUsuario(this.usuario).subscribe({
+      next: () => {
+        //alert('Usuario registrado exitosamente');
+        this.mostrarToast('Éxito', 'Usuario registrado correctamente', true);
+        //this.router.navigate(['/usuarios']); // Cambia por la ruta deseada
+        setTimeout(() => {
+          this.router.navigate(['/usuarios']);
+        }, 3000); // espera a que el toast se muestre
+      },
+      error: (error) => {
+        console.error('Error al registrar usuario:', error);
+        //alert('Error al registrar usuario');
+        this.mostrarToast('Error', 'Error al registrar usuario', false);
+        //this.toastService.setToast('Error al registrar usuario', false);
+      },
+    });
+  }
+  cancelar(): void {
+    this.router.navigate(['/usuarios']); // Redirige a la lista de personas
+  }
+
+  mostrarToast(titulo: string, mensaje: string, exito: boolean) {
+    $('#toast-title').text(titulo);
+    $('#toast-body').text(mensaje);
+
+    const header = $('#toast .toast-header');
+    header.removeClass('bg-success bg-danger text-white');
+    header.addClass(exito ? 'bg-success text-white' : 'bg-danger text-white');
+
+    $('#toast').toast('show');
+  }
 }
--- src/app/business/usuario/usuario/updateusuario/updateusuario.component.ts
+++ src/app/business/usuario/usuario/updateusuario/updateusuario.component.ts
@@ -1,12 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink, Router } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+declare var $: any;
 
 @Component({
   selector: 'app-updateusuario',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './updateusuario.component.html',
-  styleUrl: './updateusuario.component.css'
+  styleUrl: './updateusuario.component.css',
 })
-export class UpdateusuarioComponent {
+export class UpdateusuarioComponent implements OnInit {
+  usuarioId!: number;
+  usuario = {
+    username: '',
+    email: '',
+    first_name: '',
+    last_name: '',
+    rol: '',
+  };
 
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.usuarioId = Number(this.route.snapshot.paramMap.get('id'));
+    this.obtenerUsuarioPorId(this.usuarioId);
+  }
+
+  obtenerUsuarioPorId(id: number): void {
+    this.authService.obtenerUsuarioPorId(id).subscribe({
+      next: (usuario) => {
+        this.usuario = usuario;
+      },
+      error: (error) => {
+        this.mostrarToast('Error', 'No se pudo cargar el usuario', false);
+      },
+    });
+  }
+
+  actualizarUsuario() {
+    console.log('Usurio:', this.usuario);
+    this.authService
+      .actualizarUsuarioPorId(this.usuarioId, this.usuario)
+      .subscribe({
+        next: (response) => {
+          console.log('Usuario actualizado', response);
+          this.mostrarToast('Éxito', 'Usuario actualizado correctamente', true);
+          setTimeout(() => this.router.navigate(['/usuarios']), 3000);
+        },
+        error: (error) => {
+          console.error('Error al actualizar usuario:', error);
+          this.mostrarToast('Error', 'Error al actualizar usuario', false);
+        },
+      });
+  }
+
+  cancelar() {
+    this.router.navigate(['/usuarios']);
+  }
+
+  mostrarToast(titulo: string, mensaje: string, exito: boolean) {
+    $('#toast-title').text(titulo);
+    $('#toast-body').text(mensaje);
+
+    const header = $('#toast .toast-header');
+    header.removeClass('bg-success bg-danger text-white');
+    header.addClass(exito ? 'bg-success text-white' : 'bg-danger text-white');
+
+    $('#toast').toast({ autohide: true, delay: 3000 }).toast('show');
+  }
 }
